6004738a5430628f9df4db2533c7443593899ce2
michael caldo <36462489+222michael@users.noreply.github.com>
2025-08-30 00:59:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let lists = document.getElementsByClassName('list');
+let rightbox = document.getElementById('right');
+let leftbox = document.getElementById('left');
+let selected = null;
+
+for (let list of lists) {
+    list.addEventListener('dragstart', function (e) {
+       
+        selected = e.target.classList.contains('list') ? e.target : e.target.closest('.list');
+    });
+}
+
+rightbox.addEventListener('dragover', function (e) {
+    e.preventDefault();
+});
+rightbox.addEventListener('drop', function (e) {
+    if (selected) {
+        rightbox.appendChild(selected);
+        selected = null;
+    }
+});
+leftbox.addEventListener('dragover', function (e) {
+    e.preventDefault();
+});
+leftbox.addEventListener('drop', function (e) {
+    if (selected) {
+        leftbox.appendChild(selected);
+        selected = null;
+    }
+});
